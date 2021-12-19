a379d10f7585fbe7ab4c5dd539cd2a1c5aa9d3ae
Naman Sood <54349762+namansoood@users.noreply.github.com>
2021-12-19 17:30:39 +0530

    focus input on empty

--- components/Form.js
+++ components/Form.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 import styles from "./Form.module.css"
 
 export default function (props) {
     let [value, setValue] = useState("")
+    let inputRef = useRef(null)
 
     return <>
         <form className={styles.form} onSubmit={e => {
-            e.preventDefault()
+            e.preventDefault();
             if (value !== "") {
-                props.onSubmit(value)
-                setValue("")
+                props.onSubmit(value);
+                setValue("");
+            } else if (inputRef.current !== null) {
+                inputRef.current.focus();
             }
         }}>
-            <input className={styles.input} onChange={e => setValue(e.currentTarget.value)} value={value} placeholder="Paste a link to shorten it" />
+            <input ref={inputRef} className={styles.input} onChange={e => setValue(e.currentTarget.value)} value={value} placeholder="Paste a link to shorten it" />
             <button type="submit" className={styles.button}>Do Your Magic</button>
         </form>
     </>
